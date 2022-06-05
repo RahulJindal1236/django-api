32199b2f04f57560b153e0ba427d28e60c93a409
RahulJindal1236 <rahuljindal1236@gmail.com>
2022-06-05 18:50:00 +0530

    creating serializers and updating views

--- api/views.py
+++ api/views.py
@@ -1,19 +1,30 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Tasks
+from .serializers import TaskSerializer
 # Create your views here.
 
 @api_view(['GET'])
 def allTask(request):
-    task = {
-     "name":'Maths Assignment',
-     "description":'some description of the task',
-     "email":'email@gmail.com'
-    }
+    # task = {
+    #  "name":'Maths Assignment',
+    #  "description":'some description of the task',
+    #  "email":'email@gmail.com'
+    # }
 
-    return Response(task)
+    tasks = Tasks.objects.all()
+    serializer = TaskSerializer(tasks,many=True)
+
+    return Response(serializer.data)
 
 @api_view(['POST'])
 def newTask(request):
      print(request.data)
+     serializer = TaskSerializer(data=request.data)
+     if serializer.is_valid():
+         serializer.save()
      return Response('item recieved and saved successfully')
+    #  else:
+    #      print(serializer.errors)
+    #      return Response({'message':'Error occured | {serializer.errors}'})
