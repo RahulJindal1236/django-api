48cefe147cf0ebaac5f038d1202b1a137be5f100
RahulJindal1236 <rahuljindal1236@gmail.com>
2022-06-05 18:26:37 +0530

    Creating django Model - Tasks

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tasks(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    email = models.EmailField()
+    createdAt = models.DateTimeField(auto_now_add=True)
