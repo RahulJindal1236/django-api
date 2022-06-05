660520690538fe51983c15e925b41dfc1c51dc65
RahulJindal1236 <rahuljindal1236@gmail.com>
2022-06-05 18:20:37 +0530

    Creating views

--- api/views.py
+++ api/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
-from rest_framework import api_view
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 # Create your views here.
+
+@api_view(['GET'])
+def allTask(request):
+    task = {
+     "name":'Maths Assignment',
+     "description":'some description of the task',
+     "email":'email@gmail.com'
+    }
+
+    return Response(task)
+
+@api_view(['POST'])
+def newTask(request):
+     print(request.data)
+     return Response('item recieved and saved successfully')
